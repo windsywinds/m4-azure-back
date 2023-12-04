19b3d53a42628c1f56f84c00fe57bb06aaa0bede
windsywinds <windsor.sam@gmail.com>
2023-12-04 14:59:16 +1300

    no slash

--- index.cjs
+++ index.cjs
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8001;
 
 app.use(cors({
-  origin: 'https://polite-island-03d47e90f.4.azurestaticapps.net/',
+  origin: 'https://polite-island-03d47e90f.4.azurestaticapps.net',
   methods: 'GET,POST',
   credentials: false, // If you're using credentials (e.g., cookies) in your requests
 }));

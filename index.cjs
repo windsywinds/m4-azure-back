0773b6c63387593c94f65a5ac1a71eb53fd7ae56
windsywinds <windsor.sam@gmail.com>
2023-12-04 14:50:14 +1300

    update cors headers

--- index.cjs
+++ index.cjs
@@ -9,7 +9,11 @@ const getrandom = require('./routes/getRandom.cjs')
 const app = express();
 const PORT = process.env.PORT || 8001;
 
-
+app.use(cors({
+  origin: 'https://polite-island-03d47e90f.4.azurestaticapps.net/',
+  methods: 'GET,POST',
+  credentials: false, // If you're using credentials (e.g., cookies) in your requests
+}));
 
 
 
